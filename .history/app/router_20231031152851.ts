2954dc680ea328874f94bb6f3f391e4da7336488
grady <497551126@qq.com>
2023-10-31 15:44:50 +0800

    first commit

---
+++ .history/app/router_20231031152851.ts
@@ -0,0 +1,11 @@
+import { Application } from 'egg';
+
+export default (app: Application) => {
+  const { controller, router } = app;
+
+  router.redirect('/', '/news');
+  router.get('/news', controller.news.list);
+  router.get('/news/all', controller.news.all);
+  router.get('/news/item/:id', controller.news.detail);
+  router.get('/news/user/:id', controller.news.user);
+};
